2d0a839b2eedc0c38a076b17869d29962bafbe77
Chiksinafees <nafeesahmed01.na@gmail.com>
2023-07-13 16:38:47 +0530

    Basics of javascript in node.js

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+console.log("hello world");
 
-console.log('hello world')
+const product = (x, y) => {
+  return x * y;
+};
+
+console.log(product(2, 4));
+
+const obj = {
+  name: "nafees",
+  age: 24,
+  details: function () {
+    console.log("my name is", this.name, "and am", this.age, "yrs old");
+  },
+};
+
+console.log(obj);
+obj.details();
