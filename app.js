1279ec6b2d449c753fe00991f8a6808d9104d137
Chiksinafees <nafeesahmed01.na@gmail.com>
2023-07-15 18:19:49 +0530

    learning Request/ Response in node.js

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-  const http= require('http')
+const http = require("http");
 
-  const server=http.createServer((req,res)=>{
-      console.log('nafees')
-  })
+const server = http.createServer((req, res) => {
+  console.log("nafees");
 
-  server.listen(4000)
+  const url = req.url;
+  if (url === "/home") {
+    res.write("<html></html>");
+    res.write("<head><title>home page</title></head>");
+    res.write("<body><h1>WELCOME HOME</h1></body>");
+    return res.end();
+  }
+
+  if (url === "/about") {
+    res.write("<html></html>");
+    res.write("<head><title>home page</title></head>");
+    res.write("<body><h1>WELCOME TO ABOUT US PAGE</h1></body>");
+    return res.end();
+  }
+
+  if (url === "/node") {
+    res.write("<html></html>");
+    res.write("<head><title>home page</title></head>");
+    res.write("<body><h1>WELCOME TO MY NODE JS PROJECT</h1></body>");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html></html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("<body><h1>hello from my node.js server!</h1></body>");
+  res.end();
+});
+
+server.listen(4000);
