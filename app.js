8ee98860fbff8b3b62dbff0807773a4d14e59dfe
Chiksinafees <nafeesahmed01.na@gmail.com>
2023-07-25 18:52:57 +0530

    learning middleware

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 // const http = require("http");        we are using app.listen(3000) so this also not require
 
-
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded());
+app.use("/add-product", (req, res, next) => {
   console.log("1st middleware");
-  next();
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"></input><input type="number" name="size"><button type="submit">Add Product</button></input></form>'
+  );
 });
-app.use((req, res, next) => {
-  console.log("2nd middleware");
-  res.send({ name: "nafees" });
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+}); 
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>learning Express.js</h1>");
 });
 
 app.listen(3000); // as app.listen(3000)  = http.createServer(app)
-                                       //   server.listen(3000)
+//   server.listen(3000)
 
 // const server = http.createServer(app)
 // server.listen(3000);
