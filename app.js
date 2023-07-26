01f59a0bffa3a6f9fd0e104dfd734123a8d73a32
Chiksinafees <nafeesahmed01.na@gmail.com>
2023-07-26 12:56:48 +0530

    using Routers and Filters

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
-// const http = require("http");        we are using app.listen(3000) so this also not require
-
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded());
-app.use("/add-product", (req, res, next) => {
-  console.log("1st middleware");
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"></input><input type="number" name="size"><button type="submit">Add Product</button></input></form>'
-  );
+
+app.use("/admin", adminRoutes);
+app.use("/admin", shopRoutes);
+
+app.use((req, res) => {
+  res.status(404).send("<h1>Page not found !</h1>");
 });
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-}); 
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>learning Express.js</h1>");
-});
-
-app.listen(3000); // as app.listen(3000)  = http.createServer(app)
-//   server.listen(3000)
-
-// const server = http.createServer(app)
-// server.listen(3000);
+app.listen(3000);
